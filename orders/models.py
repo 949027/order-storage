1986e5ce78466d62d2701a8b70d8ccc4e6e49164
949027 <82878668+949027@users.noreply.github.com>
2022-08-16 00:24:41 +0700

    Added order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    number = models.IntegerField('Номер заказа')
+    usd_price = models.DecimalField(
+        max_digits=7,
+        decimal_places=2
+    )
+    rub_price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2
+    )
+    delivery_date = models.DateTimeField('Дата поставки')
