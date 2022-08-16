08c3a1a1918e30c7f7da468b91617ecd422b56d7
949027 <82878668+949027@users.noreply.github.com>
2022-08-16 17:31:15 +0700

    Admin panel configured

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'usd_price',
+        'rub_price',
+        'delivery_date',
+        'is_overdue',
+        'expiration_message_sent',
+    ]
