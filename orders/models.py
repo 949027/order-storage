b3c4f8d9cbdeb5fd0e8716dbebf093160c6b1228
949027 <82878668+949027@users.noreply.github.com>
2022-08-16 22:23:44 +0700

    Fixed numeric field overflow

--- orders/models.py
+++ orders/models.py
@@ -4,11 +4,11 @@ from django.db import models
 class Order(models.Model):
     id = models.IntegerField('Номер заказа', primary_key=True)
     usd_price = models.DecimalField(
-        max_digits=7,
+        max_digits=20,
         decimal_places=2
     )
     rub_price = models.DecimalField(
-        max_digits=8,
+        max_digits=20,
         decimal_places=2,
     )
     delivery_date = models.DateField('Дата поставки')
