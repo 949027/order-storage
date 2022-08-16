36e4e9c6a21b6b1bfb9bc12b815631a7953ecc86
949027 <82878668+949027@users.noreply.github.com>
2022-08-17 01:38:24 +0700

    Repeating requests fixed

--- orders/management/commands/watch.py
+++ orders/management/commands/watch.py
@@ -6,7 +6,6 @@ from bs4 import BeautifulSoup
 from django.core.management.base import BaseCommand
 import google.auth
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
 from order_storage import settings
 from orders.models import Order
@@ -53,34 +52,34 @@ class Command(BaseCommand):
         3) добавляет/обновляет/удаляет запись в БД;
         4) отправляет предупреждение в телеграм если истек срок поставки.
         """
-        try:
-            bot = telegram.Bot(token=settings.TELEGRAM_TOKEN)
-            currency_price = get_currency_price(settings.CURRENCY_ID)
-            sheet = get_sheet(settings.SPREADSHEET_ID)
-        except HttpError as error:
-            print(f'An error occurred: {error}')
-            return error
+        bot = telegram.Bot(token=settings.TELEGRAM_TOKEN)
 
         while True:
-            order_ids = []
-            for row in sheet:
-                delivery_date = datetime.strptime(row[3], '%d.%m.%Y').date()
-                usd_price = float(row[2])
-                order, _ = Order.objects.update_or_create(
-                    id=row[1],
-                    defaults={
-                        'usd_price': usd_price,
-                        'rub_price': usd_price * currency_price,
-                        'delivery_date': delivery_date,
-                        'is_overdue': True if datetime.now().date() > delivery_date else False
-                    }
-                )
-                order_ids.append(order.id)
-                if order.is_overdue and not order.expiration_message_sent:
-                    send_alert(bot, order.id)
-                    order.expiration_message_sent = True
-                    order.save()
+            try:
+                currency_price = get_currency_price(settings.CURRENCY_ID)
+                sheet = get_sheet(settings.SPREADSHEET_ID)
 
-            Order.objects.exclude(id__in=order_ids).delete()
+                order_ids = []
+                for row in sheet:
+                    delivery_date = datetime.strptime(row[3], '%d.%m.%Y').date()
+                    usd_price = float(row[2])
+                    order, _ = Order.objects.update_or_create(
+                        id=row[1],
+                        defaults={
+                            'usd_price': usd_price,
+                            'rub_price': usd_price * currency_price,
+                            'delivery_date': delivery_date,
+                            'is_overdue': True if datetime.now().date() > delivery_date else False
+                        }
+                    )
+                    order_ids.append(order.id)
+                    if order.is_overdue and not order.expiration_message_sent:
+                        send_alert(bot, order.id)
+                        order.expiration_message_sent = True
+                        order.save()
 
-            time.sleep(settings.DELAY_TIME)
+                Order.objects.exclude(id__in=order_ids).delete()
+
+                time.sleep(settings.DELAY_TIME)
+            except:
+                continue
