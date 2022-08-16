7be98b6f47e5a35bc3d7e2344e5a7eed65921704
949027 <82878668+949027@users.noreply.github.com>
2022-08-16 15:48:53 +0700

    Added fields for send message to telegram

--- orders/models.py
+++ orders/models.py
@@ -2,13 +2,18 @@ from django.db import models
 
 
 class Order(models.Model):
-    number = models.IntegerField('Номер заказа')
+    id = models.IntegerField('Номер заказа', primary_key=True)
     usd_price = models.DecimalField(
         max_digits=7,
         decimal_places=2
     )
     rub_price = models.DecimalField(
         max_digits=8,
-        decimal_places=2
+        decimal_places=2,
     )
-    delivery_date = models.DateTimeField('Дата поставки')
+    delivery_date = models.DateField('Дата поставки')
+    is_overdue = models.BooleanField('Срок истек', default=False)
+    expiration_message_sent = models.BooleanField(
+        'Уведомление о просрочке отправлено',
+        default=False,
+    )
